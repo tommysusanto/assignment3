1e7c5ac2aa99a99e49ae4139b95e54d898d2ab11
Tommy Susanto <tommy88susanto@gmail.com>
2016-04-19 19:31:58 +1000

    init

---
+++ ga-assignment3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ga-assignment3
+//
+//  Created by Tommy Susanto on 19/04/2016.
+//  Copyright © 2016 Tommy Susanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
